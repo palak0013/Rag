7917e94b79814f6fca629bb54d3c87e798a2a0e9
Palak Singh <2023baiml034@axiscolleges.in>
2026-03-29 11:35:58 +0530

    fixed embedding model

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,10 +1,15 @@
 #%%
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Chroma
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 def create_vectorstore(chunks):
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/gemini-embedding-001"   # ✅ correct
+        model="models/gemini-embedding-001",
+        google_api_key=os.getenv("GOOGLE_API_KEY")  # ✅ correct
     )
 
     vectorstore = Chroma.from_documents(
